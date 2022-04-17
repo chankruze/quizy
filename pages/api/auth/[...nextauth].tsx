be6bdb1d8463a45f460d232eee5790952056a02a
chankruze <chankruze@gmail.com>
2022-04-17 21:41:18 +0530

    feat: enable github login
    Signed-off-by: chankruze <chankruze@gmail.com>

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -6,9 +6,13 @@ Copyright (c) geekofia 2022 and beyond
 */
 
 import NextAuth from "next-auth";
+// providers
 import EmailProvider from "next-auth/providers/email";
 import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
+// adapter
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+// mongodb connection
 import clientPromise from "../../../lib/mongodb";
 
 export default NextAuth({
@@ -32,6 +36,10 @@ export default NextAuth({
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
 });
