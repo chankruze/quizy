95972bdebd0ed3e7fe7946414c2dcdade33ba058
chankruze <chankruze@gmail.com>
2022-04-22 08:43:11 +0530

    remove facebook login
    Signed-off-by: chankruze <chankruze@gmail.com>

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -8,7 +8,6 @@ Copyright (c) geekofia 2022 and beyond
 import NextAuth from "next-auth";
 // providers
 import EmailProvider from "next-auth/providers/email";
-import FacebookProvider from "next-auth/providers/facebook";
 import GitHubProvider from "next-auth/providers/github";
 // adapter
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
@@ -32,10 +31,6 @@ export default NextAuth({
         },
       },
       from: process.env.EMAIL_FROM,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID as string,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
