fcc6b3644767ae2f72097d84490508d8421a0f5b
chankruze <chankruze@gmail.com>
2022-04-24 20:06:36 +0530

    fix: the code to check student name
    Signed-off-by: chankruze <chankruze@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,48 @@ const Home = ({ student }: Props) => {
       <main className="flex flex-wrap-reverse w-full flex-1 py-2 px-2 sm:px-4">
         <div className="md:flex-1 flex justify-end">
           <div className="text-center lg:text-left md:m-auto px-4 md:px-10">
-            {student && student.verification !== "verified" ? (
+            {student && student.verification === "verified" ? (
+              <>
+                <h1
+                  className="text-4xl tracking-tight leading-10 font-extrabold font-poppins 
+                text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl"
+                >
+                  {/* Welcome to <span className="text-green-500">{config.APP_NAME}</span> */}
+                  Welcome back,
+                  <br />
+                  <span className="text-blue-600 text-5xl">
+                    {student.bioData.name}
+                  </span>
+                </h1>
+                <p className="mt-3 text-base text-gray-700 font-nunito sm:mt-4 sm:text-xl lg:text-lg xl:text-xl">
+                  Always keep your bio data updated ;)
+                </p>
+                <div className="mt-4 flex gap-4">
+                  <div className="md:inline-block">
+                    <Link href="/quiz" passHref>
+                      <a
+                        className="w-full md:w-auto flex items-center justify-center text-base 
+                      leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 duration-150 ease-in-out cursor-pointer 
+                      py-3 px-8 md:py-4 md:text-lg md:px-10 mx-auto lg:mx-0"
+                      >
+                        Attend Quizzes
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="md:inline-block">
+                    <Link href="/bio-data" passHref>
+                      <a
+                        className="w-full md:w-auto flex items-center justify-center text-base 
+                      leading-6 font-medium rounded-md bg-white shadow-md hover:bg-gray-100 duration-150 
+                      ease-in-out cursor-pointer py-3 px-8 md:py-4 md:text-lg md:px-10 mx-auto lg:mx-0"
+                      >
+                        Update Bio Data
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              </>
+            ) : (
               <>
                 <h1
                   className="text-4xl tracking-tight leading-10 font-extrabold font-poppins 
@@ -39,47 +80,6 @@ const Home = ({ student }: Props) => {
                 >
                   Get Started
                 </button>
-              </>
-            ) : (
-              <>
-                <h1
-                  className="text-4xl tracking-tight leading-10 font-extrabold font-poppins 
-                  text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl"
-                >
-                  {/* Welcome to <span className="text-green-500">{config.APP_NAME}</span> */}
-                  Welcome back,
-                  <br />
-                  <span className="text-blue-600 text-5xl">
-                    {student.bioData.name}
-                  </span>
-                </h1>
-                <p className="mt-3 text-base text-gray-700 font-nunito sm:mt-4 sm:text-xl lg:text-lg xl:text-xl">
-                  Always keep your bio data updated ;)
-                </p>
-                <div className="mt-4 flex gap-4">
-                  <div className="md:inline-block">
-                    <Link href="/quiz" passHref>
-                      <a
-                        className="w-full md:w-auto flex items-center justify-center text-base 
-                        leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 duration-150 ease-in-out cursor-pointer 
-                        py-3 px-8 md:py-4 md:text-lg md:px-10 mx-auto lg:mx-0"
-                      >
-                        Attend Quizzes
-                      </a>
-                    </Link>
-                  </div>
-                  <div className="md:inline-block">
-                    <Link href="/bio-data" passHref>
-                      <a
-                        className="w-full md:w-auto flex items-center justify-center text-base 
-                        leading-6 font-medium rounded-md bg-white shadow-md hover:bg-gray-100 duration-150 
-                        ease-in-out cursor-pointer py-3 px-8 md:py-4 md:text-lg md:px-10 mx-auto lg:mx-0"
-                      >
-                        Update Bio Data
-                      </a>
-                    </Link>
-                  </div>
-                </div>
               </>
             )}
           </div>
